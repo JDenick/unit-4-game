e19265c3951cb8a4a0a678011523fa7e20e6ce2a
Jesse Denick <jessedenick@gmail.com>
2019-07-19 14:18:45 -0400

    created variables and clicked on links

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,51 @@
 alert("test");
+
+// GLOBAL VARIABLES ****************
+var crystal = {
+    diamond:
+    {
+        name: "diamond",
+        value: 0
+    },
+    ruby:
+    {
+        name: "ruby",
+        value: 0
+    },
+    emerald:
+    {
+        name: "emerald",
+        value: 0
+    },
+    amber:
+    {
+        name: "amber",
+        value: 0
+    }
+};
+
+var currentScore = 0;
+var targetScore = 0;
+
+var winCount = 0;
+var lossCOunt = 0;
+
+
+// FUNCTIONS ********************
+
+
+
+
+// MAIN PROCESS ********************
+$("#diamond").click(function(){
+    alert("diamond");
+});
+$("#ruby").click(function(){
+    alert("ruby");
+});
+$("#emerald").click(function(){
+    alert("emerald");
+});
+$("#amber").click(function(){
+    alert("amber");
+});
